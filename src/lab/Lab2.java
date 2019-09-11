83dd24af4b734d46aa0e51bd86d17d30b7d6521e
rcchan5 <RoshCChan@gmail.com>
2019-09-11 00:15:09 -0400

    Completed Assignment with all extra credit tasks completed

--- src/lab/Lab2.java
+++ src/lab/Lab2.java
@@ -19,6 +19,7 @@ public class Lab2
 		"phenylalanine", "proline", 
 		"serine","threonine","tryptophan", 
 		"tyrosine", "valine"};
+	
 	public static Boolean timer(long timeLimit, long started)
 	{
 		if (timeLimit+started <= System.currentTimeMillis())
@@ -32,61 +33,83 @@ public class Lab2
 		}
 		
 	}
-	public static int quiz(long timeLimit,int totalQuestions) 
+	public static long elapse(long started) 
 	{
-		
+		long current = (System.currentTimeMillis()-started)/1000;
+		return current;
+	}
+	
+	public static void quiz(long timeLimit,int totalQuestions, int [] summary) 
+	{
+		Scanner input1 = new Scanner(System.in);  // Create a Scanner object
 		Random random = new Random();
 		int right=0;
-		int wrong=0;
 		long started = System.currentTimeMillis();
-		while (timer(timeLimit,started)) 
+		int questionCounter =0;
+		
+		
+		while (timer(timeLimit,started) && questionCounter!=totalQuestions) 
 		{
 			int c1 =random.nextInt(19);
+			questionCounter++;
+			System.out.println("\n-------------Question"+questionCounter+"-----------------");
 			System.out.println(FULL_NAMES[c1]);
-			Scanner input1 = new Scanner(System.in);  // Create a Scanner object
-
-			String ua =input1.next();
+			String ua =input1.next().toUpperCase();
 			
-			ua=ua.toUpperCase();
-			if (ua.equals(SHORT_NAMES))
+			if (ua.equals("QUIT")) 
 			{
-				System.out.println("Correct!");
+				System.out.println("Good Bye!");
+				break;
+			}
+			else if (ua.equals(SHORT_NAMES[c1]))
+			{
 				right++;
+				System.out.println("Correct! Score: "+right+" Time Elapsed: "+elapse(started)+" out of "+timeLimit/1000 +" seconds");
+				
 			}
 			else 
 			{
-				wrong++;
+				
+				System.out.println("Wrong! Score: "+right+" Time Elapsed: "+elapse(started)+" out of "+timeLimit/1000+" seconds");
+				summary[c1]++;
 			}
-			//input1.close();
+		
 			
 		}
+		input1.close();
 		
-		return 1;
+		
+		// End of game user summary
+		System.out.println("----------------------------------------");
+		System.out.println("You final score was "+right);
+		System.out.println("You got these wrong:");
+		for (int i=0; i<summary.length;i++)
+		{
+			if(summary[i] != 0) 
+			{
+				System.out.println(FULL_NAMES[i]+" wrong "+summary[i]+" time(s)");
+			}
+			
+		}
+				
 	}
 	public static void main(String[] args)
 	{
+		
+		// getting user input
 	    Scanner input = new Scanner(System.in);  // Create a Scanner object
 	    System.out.println("How long do you want the game to run?(seconds) ");
-	    Long timeLimit = (input.nextLong()*1000);
+	    Long timeLimit = (input.nextLong()*1000);// multiply by 1000 due to currentTimeMillis gets time in milliseconds
 	    System.out.println("How many questions do you want? ");
 	    Integer totalQuestions = input.nextInt();
-	    //input.close();
-	    quiz(timeLimit,totalQuestions);
+	    
+	    //array used to store wrong answers
+	    int[] summary = new int[20];
+	    
+	    quiz(timeLimit,totalQuestions, summary);
+	    input.close();
+	    	
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 }
