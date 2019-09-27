6ba1c803fe01bdf9cc4c7dcc6746162c0858fcbe
rcchan5 <RoshCChan@gmail.com>
2019-09-27 13:31:35 -0400

    initial

--- src/lab/Lab4/ExtraCredit.java
+++ src/lab/Lab4/ExtraCredit.java
@@ -1,5 +1,18 @@
 package lab.Lab4;
+/*
+ * For extra-credit:
 
+Create a class that allows a user to parse an arbitrarily large Fasta file
+one sequence at a time but does not hold the sequences in memory.
+
+This class should have a function called something like
+
+	public FastaSequence getNextSequence()
+
+that returns the next FastaSequence in the file or null if there are
+no more sequences…
+
+ */
 public class ExtraCredit
 {
 
