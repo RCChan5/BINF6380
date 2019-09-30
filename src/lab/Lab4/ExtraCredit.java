13dc126756c5975c66874e6fe481bcf8eb05be94
rcchan5 <RoshCChan@gmail.com>
2019-09-29 23:00:56 -0400

    first iteration structure completed

--- src/lab/Lab4/ExtraCredit.java
+++ src/lab/Lab4/ExtraCredit.java
@@ -1,4 +1,14 @@
 package lab.Lab4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
 /*
  * For extra-credit:
 
@@ -13,7 +23,99 @@ that returns the next FastaSequence in the file or null if there are
 no more sequences…
 
  */
+
+
 public class ExtraCredit
 {
+	//object needs to store position and changes it every time the next() method is called 
+	//constructor
+	public ExtraCredit(String location) throws Exception
+	{
+		BufferedReader reader = new BufferedReader(new FileReader(new File(location)));
+
+		//using linked hash map just in case order of fasta file matter
+		LinkedHashMap<String, String> hash_map = new LinkedHashMap<String, String>();
+		String key = null;
+		
+		//
+		//*****instead of sing this for loop keep it pointng at the start till nextseq  
+		//i dont know bufferedREader as well ask later
+		for(String nextLine =reader.readLine(); nextLine != null;nextLine=reader.readLine()) 
+		{	
+
+			if (nextLine.startsWith(">")) 
+			{	
+				key = nextLine;
+				//System.out.println(key);
+			}
+			else
+			{
+				//if else is for multi line fasta files so null value is not appended into value
+				if(hash_map.get(key)==null) {
+					hash_map.put(key,nextLine);
+				}
+				else {
+					hash_map.put(key,hash_map.get(key)+nextLine);
+				}
+			}
+			
+		}	
+		System.out.println("test");
+	
+	}
+/*
+ * method to just print sequence to text file
+ */
+public static void fileWriter(ArrayList<String> results, String location) throws Exception
+{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(location)));
+		
+					
+		writer.flush();
+		writer.close();
+}
+
+public static String getNextFastaSequence() 
+{	
+	BufferedReader reader = new BufferedReader(new FileReader(new File(location)));
+	
+	//using linked hash map just in case order of fasta file matter
+	LinkedHashMap<String, String> hash_map = new LinkedHashMap<String, String>();
+	String key = null;
+	
+	//parses fasta file into linked hashmap
+	for(String nextLine =reader.readLine(); nextLine != null;nextLine=reader.readLine()) 
+	{	
+
+		if (nextLine.startsWith(">")) 
+		{	
+			key = nextLine;
+			//System.out.println(key);
+		}
+		else
+		{
+			//if else is for multi line fasta files so null value is not appended into value
+			if(hash_map.get(key)==null) {
+				hash_map.put(key,nextLine);
+			}
+			else {
+				hash_map.put(key,hash_map.get(key)+nextLine);
+			}
+		}
+		
+	}	
+	
+	
+	return null;	
+}
+	
+public static void main(String[] args)
+{
+	//while(getNextFastaSequence()!=null) 
+	
+	ExtraCredit x1 = new ExtraCredit();
+		
+	
+}
 
 }
