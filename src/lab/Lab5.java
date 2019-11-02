e7a4d93d96f5aa35d5173fb7e17bab7121d4f955
rcchan5 <RoshCChan@gmail.com>
2019-11-02 14:35:00 -0400

    Save to transfer over

--- src/lab/Lab5.java
+++ src/lab/Lab5.java
@@ -1,71 +1,77 @@
 package lab;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.*;
 
 
-public class Lab5 extends JFrame {
+public class Lab5 extends JFrame 
+{
 
-    private JPanel contentPane;
-    private JTextField DisplayOne;
-    
-
-    int count;
-    int count1;
-    int count2;
-
-
-
-
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() 
-            {
-                  try {
-                    Lab5 frame = new Lab5();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-
-
-    public Lab5() 
-    {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(1000, 1000, 200, 200);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(new BorderLayout(0, 0));
-        setContentPane(contentPane);
-
-        JPanel panel = new JPanel();
-        contentPane.add(panel, BorderLayout.CENTER);
-
-        DisplayOne = new JTextField();
-        panel.add(DisplayOne);
-        DisplayOne.setColumns(10);
-
-
-        JButton btnCountOne = new JButton("Count");
-        count1=0;
-        btnCountOne.addActionListener(new ActionListener() {                
-                public void actionPerformed(ActionEvent arg0) 
-                {   
-                    DisplayOne.setText(Integer.toString(count1++));
-                }
-        });
-        panel.add(btnCountOne);
-    }
+	private static final long serialVersionUID = 1L;
+	//basic frame
+	private JTextField aTextField = new JTextField();
+	//new button with name
+	private JButton doubleButton = new JButton("JButton");
+	private JButton sad = new JButton("sad");
+	private JButton das = new JButton("das");
+	private int numDollars=1;
+	private Random random = new Random();
+	
+	private class DoubleActionListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent arg0)
+		{
+			if(random.nextFloat() < .5) 
+			{
+				numDollars = numDollars*2;
+			}
+			else 
+			{
+				numDollars= 0;
+			}
+			updateTextField();
+		}
+	}
+	
+	private void updateTextField()
+	{
+		aTextField.setText("part 2 You have $"+numDollars);
+	}
+	
+	private JPanel getBottomPanel()
+	{
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(0,3));
+		panel.add(doubleButton);
+		panel.add(sad);
+		panel.add(das);
+		
+		doubleButton.addActionListener(new DoubleActionListener());
+		return panel;
+	}
+	public Lab5()
+	{
+		super("Double Your Money!");
+		setLocationRelativeTo(null);
+		setSize(200,200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(getBottomPanel(), BorderLayout.SOUTH);
+		//connects button to its function
+		doubleButton.addActionListener(new DoubleActionListener());
+		
+		getContentPane().add(aTextField, BorderLayout.CENTER);
+		aTextField.setText("You have $"+numDollars);
+		setVisible(true);
+	}
+	public static void main(String[] args) 
+	{
+		new Lab5();
+	}
+   
 
 }
